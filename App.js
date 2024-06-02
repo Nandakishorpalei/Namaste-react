e7c5fd09e87624d1d0584d66ba2934cd0fc95273
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2024-06-02 13:03:02 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is namaste react 🚀!"
-);
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2" },
-  "By Nanda kishor palei!"
-);
+//jsx => Babel transpiles it to React.createElement => ReactElement- JS object => HTMLElement(render on browser)
+const Title = () => <h1 id="heading">Namaste react from jsx 🚀</h1>;
 
-const child = React.createElement("div", {}, [heading, heading2]);
-const parent = React.createElement("div", { id: "parent" }, [child, child]); //object
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
